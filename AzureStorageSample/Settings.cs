179ec6f32c3040022dbe89108e17f82fe50369e9
Andres Pineda <pineda.andres@gmail.com>
2017-01-15 20:26:15 -0500

    Add settings place holders

--- AzureStorageSample/Settings.cs
+++ AzureStorageSample/Settings.cs
@@ -3,8 +3,8 @@
     // Para simplificar el ejemplo.
     public static class Settings
     {
-        public static readonly string AzureKey = "";
+        public static readonly string AzureKey = "REPLACE_WITH_YOUR_AZURE_SSA_KEY";
 
-        public static readonly string ImageContainer = "";
+        public static readonly string ImageContainer = "REPLACE_WITH_YOUR_CONTAINER_NAME";
     }
 }
